f86c8c12e3112faf838f13342c5db1755f5c217b
Luka Magics <opro1226@naver.com>
2023-03-20 10:34:11 +0900

    initial commit

---
+++ torch_iris_handler.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn.functional as F
+
+class PyTorchIrisHandler:
+    def __init__(self):
+        # Load the PyTorch model
+        self.model = torch.load('/home/jovyan/iris/model-archiver/torch_iris.pth')
+        self.model.eval()
+        
+    def preprocess(self, data):
+        # Parse the input data
+        input_data = torch.tensor(data).float()
+        return input_data
+    
+    def inference(self, input_data):
+        # Make predictions
+        with torch.no_grad():
+            output = self.model(input_data)
+        return output
+    
+    def postprocess(self, inference_output):
+        # Convert PyTorch output tensor to a NumPy array
+        output = inference_output.numpy().tolist()
+        return {'output': output}
---
+++ torch_iris_training.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+iris = load_iris()
+X = iris['data']
+y = iris['target']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=2)
+X_train = torch.FloatTensor(X_train)
+X_test = torch.FloatTensor(X_test)
+y_train = torch.LongTensor(y_train)
+y_test = torch.LongTensor(y_test)
+
+class Model(nn.Module):
+    def __init__(self, input_dim):
+        super(Model, self).__init__()
+        self.layer1 = nn.Linear(input_dim, 50)
+        self.layer2 = nn.Linear(50, 50)
+        self.layer3 = nn.Linear(50, 3)
+        
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        x = F.softmax(self.layer3(x), dim=1)
+        return x
+    
+model     = Model(X_train.shape[1])
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+loss_fn   = nn.CrossEntropyLoss()
+
+EPOCHS  = 100
+loss_list     = np.zeros((EPOCHS,))
+accuracy_list = np.zeros((EPOCHS,))
+
+for epoch in range(EPOCHS):
+    y_pred = model(X_train)
+    loss = loss_fn(y_pred, y_train)
+    loss_list[epoch] = loss.item()
+    
+    # Zero gradients
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+    with torch.no_grad():
+        y_pred = model(X_test)
+        correct = (torch.argmax(y_pred, dim=1) == y_test).type(torch.FloatTensor)
+        accuracy_list[epoch] = correct.mean()
+        
+print("last 5 epochs accuracy = ", accuracy_list[-5:])
+
+torch.save(model, '/home/jovyan/iris/model-archiver/torch_iris.pth')
